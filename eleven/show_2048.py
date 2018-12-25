6bf3e8574ae3d840f909d05959ec5e95bb710c0f
Bohdan Budnyy <bohdanxbudnyy@gmail.com>
2018-12-25 18:49:25 +0200

    display 2048 board in jupyter notebook

---
+++ eleven/show_2048.py
@@ -0,0 +1,86 @@
+from copy import copy
+import numpy as np
+from matplotlib import pyplot as plt
+
+
+# based on
+# https://github.com/anujgupta82/Musings
+# https://matplotlib.org/gallery/images_contours_and_fields/image_annotated_heatmap.html
+
+# for the dynamic matplotlib figure updates to work one needs to cast
+# %matplotlib notebook
+# magic in the notebook before plotting the figure
+
+class Display:
+
+    def __init__(self, state_size=(4, 4), display_size=(8, 8),
+                 colormap="magma_r", background_color="grey",
+                 textcolors=("black", "white"), text_threshold=None):
+        self.cmap = copy(plt.cm.get_cmap(colormap))
+        self.cmap.set_bad(background_color)
+
+        self.fig, ax = plt.subplots(figsize=display_size)
+
+        # remove frame and ticks
+        ax.set_frame_on(False)
+        ax.tick_params(axis="both", which="both",
+                bottom=False, top=False, left=False, right=False,
+                labelbottom=False, labeltop=False,
+                labelleft=False, labelright=False)
+
+        # set up the grid
+        ax.set_xticks(np.arange(state_size[1]+1)-.5, minor=True)
+        ax.set_yticks(np.arange(state_size[0]+1)-.5, minor=True)
+        ax.grid(which="minor", color=background_color, linestyle='-',
+                linewidth=8)
+
+        self.ax = ax
+
+        # text color change threshold (show light text on dark background and
+        # vice versa
+        self.text_threshold = text_threshold
+
+        self.text_kw = {'horizontalalignment': 'center',
+                        'verticalalignment': 'center', 'fontsize': 16}
+        self.textcolors = textcolors
+        self.texts = []
+
+        empty_state = np.zeros(state_size, dtype=int)
+        self.draw(empty_state)
+
+    def draw(self, state):
+        # value 0 is masked in state so it is shown in background color on the
+        # plot (because of call to colormap set_bad() method in init)
+        self.ax.imshow(np.ma.masked_equal(state, 0), cmap=self.cmap,
+                       vmin=0, vmax=16)
+        self.annotate(state)
+        self.fig.canvas.draw()
+
+    def annotate(self, state):
+        for text in self.texts:
+            text.remove()
+        self.texts.clear()
+        for i in range(state.shape[0]):
+            for j in range(state.shape[1]):
+                if state[i, j] > 0:
+                    # first axis in matrix is y axis on the plot,
+                    # and second axis in matrix is x axis on the plot
+                    if self.text_threshold is None:
+                        text_color = self.textcolors[0]
+                    else:
+                        text_color = self.textcolors[state[i, j] >
+                                                     self.text_threshold]
+                    text = self.ax.text(j, i, 1 << state[i, j],
+                                        color=text_color, **self.text_kw)
+                    self.texts.append(text)
+
+
+def test():
+    import time
+
+    display = Display(text_threshold=10)
+    for i in range(4):
+        time.sleep(2)
+        state = np.random.randint(17, size=(4, 4), dtype=int)
+        display.draw(state)
+
