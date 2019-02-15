f03aed10453bfb69a7e43daf0d681ceda5a936e6
Bohdan Budnyy <bohdanxbudnyy@gmail.com>
2019-02-15 23:30:48 +0200

    implemented move rules for 2048

--- 2048/game.py
+++ 2048/game.py
@@ -4,25 +4,39 @@ UP, DOWN, LEFT, RIGHT = range(4)
 
 class Game:
 
+    action_space = (UP, DOWN, LEFT, RIGHT)
+    empty_cell = 0
+
     def __init__(self, size=4):
         self.size = size
-        self.state = [0] * size * size
+        self.state = [self.empty_cell] * size * size
         self.history = []
-        self.action_space = (UP, DOWN, LEFT, RIGHT)
         self.indices = {}
-        # TODO replace with iterators?
-        rows = [tuple([i * 4 + j for j in range(self.size)])
-                for i in range(self.size)]
-        self.indices[RIGHT] = tuple(rows)
-        self.indices[LEFT] = tuple([tuple(reversed(row)) for row in rows])
-        columns = [tuple([i + j * 4 for j in range(self.size)])
-                   for i in range(self.size)]
-        self.indices[DOWN] = tuple(columns)
-        self.indices[UP] = tuple([tuple(reversed(col)) for col in columns])
+        rows = [tuple([i * size + j for j in range(size)])
+                for i in range(size)]
+        columns = [tuple([i + j * size for j in range(size)])
+                   for i in range(size)]
+        self.indices[UP] = tuple(columns)
+        self.indices[DOWN] = tuple([tuple(reversed(col)) for col in columns])
+        self.indices[LEFT] = tuple(rows)
+        self.indices[RIGHT] = tuple([tuple(reversed(row)) for row in rows])
 
     def interact(self, action):
         # change the state according to the action
-        pass
+        for sequence_indices in self.indices[action]:
+            stop_index = sequence_indices[0]
+            for i in sequence_indices[1:]:
+                cell = self.state[i]
+                if cell == self.empty_cell:
+                    continue
+                self.state[i] = self.empty_cell
+                if self.state[stop_index] == self.empty_cell:
+                    self.state[stop_index] = cell
+                elif self.state[stop_index] == cell:
+                    self.state[stop_index] += 1
+                else:
+                    stop_index += 1
+                    self.state[stop_index] = cell
 
     def observe(self):
         # return information about the state
